ef81644988dc03e2de6218355649376ead35a215
tyleranthony11 <tmanthony@mun.ca>
2025-06-10 14:14:55 -0230

    added login UI

---
+++ src/components/LoginModal.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  TextField,
+  Box,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import "./LoginModal.css";
+
+function LoginModal({ open, onClose }) {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    onClose();
+    navigate("/dashboard");
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{ className: "login-dialog" }}
+    >
+      <DialogTitle className="login-title">Employee Login</DialogTitle>
+      <DialogContent>
+        <Box component="form" noValidate className="login-form">
+          <TextField label="Username" fullWidth autoFocus />
+          <TextField label="Password" type="password" fullWidth />
+        </Box>
+      </DialogContent>
+      <DialogActions className="login-actions">
+        <Button onClick={onClose} className="login-cancel">
+          Cancel
+        </Button>
+        <Button onClick={handleLogin} className="login-submit">
+          Login
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
+
+export default LoginModal;
+
+
--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -1,24 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import LoginModal from "./LoginModal";
 import { FaPencilAlt, FaUser } from "react-icons/fa"; 
 import "./TopNav.css";
 
 function TopNav() {
-    return (
-        <div className="top-nav">
-            <div className="top-nav-content">
-                <Link to="/PostAdForm" className="top-nav-link">
-                    <FaPencilAlt className="top-nav-icon" />
-                    Post an Ad
-                </Link>
-                <div className="top-nav-separator" />
-                <Link to="/login" className="top-nav-link">
-                    <FaUser className="top-nav-icon" />
-                    Login
-                </Link>
-            </div>
-        </div>
-    );
+  const [openLogin, setOpenLogin] = useState(false);
+
+  return (
+    <div className="top-nav">
+      <div className="top-nav-content">
+        <Link to="/PostAdForm" className="top-nav-link">
+          <FaPencilAlt className="top-nav-icon" />
+          Post an Ad
+        </Link>
+
+        <div className="top-nav-separator" />
+
+        <button 
+          className="login-link" 
+          onClick={() => setOpenLogin(true)} 
+          type="button"  
+        >
+          <FaUser className="top-nav-icon" />
+          Login
+        </button>
+      </div>
+
+      <LoginModal open={openLogin} onClose={() => setOpenLogin(false)} />
+    </div>
+  );
 }
 
 export default TopNav;
