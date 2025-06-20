aebd959d99ff10bd321de3b9d9f528dbd9bb5e03
tyleranthony11 <tmanthony@mun.ca>
2025-06-20 09:29:01 -0230

    updated MTD deals to show current month

--- src/pages/dashboard/FundedDeals.jsx
+++ src/pages/dashboard/FundedDeals.jsx
@@ -36,7 +36,7 @@ const FundedDeals = () => {
 
   const parseLocalDate = (dateStr) => {
     const [year, month, day] = dateStr.split("-").map(Number);
-    return new Date(year, month - 1, day); // month is 0-indexed
+    return new Date(year, month - 1, day);
   };
 
   const mtdDeals = deals.filter((deal) => {
