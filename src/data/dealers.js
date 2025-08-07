ee7b7f1414d597a13ddd8d436fa75ccff55821ec
tyleranthony11 <tmanthony@mun.ca>
2025-08-07 11:56:46 -0230

    changed dealers.js to be in alphabaetical order

--- src/data/dealers.js
+++ src/data/dealers.js
@@ -1,58 +1,8 @@
 const dealers = {
-  "NLP Finance Marketplace": {
-    name: "NLP Finance Marketplace",
-    logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://nlpfinance.ca/",
-    location: "Grand Falls-Windsor, NL",
-  },
-  "MF Motorsports": {
-    name: "MF Motorsports",
-    logo: "/images/dealers/mf-motors.png",
-    website: "https://www.mfmotorsltd.com/",
-    location: "Stephenville, NL",
-  },
-  "Blue Water Marine": {
-    name: "Blue Water Marine",
-    logo: "/images/dealers/blue-water-marine.avif",
-    website: "https://bluewatermarine.ca/",
-    location: "Mount Pearl, NL",
-  },
-
-  "Mid Island Motorsports": {
-    name: "Mid Island Motorsports",
-    logo: "/images/dealers/mid-island-motorsports.png",
-    website: "https://www.midislandmotorsports.com/",
-    location: "Springdale, NL",
-  },
-
   "Ashford Sales": {
     name: "Ashford Sales",
     logo: "/images/dealers/nlp-finance-marketplace.png",
     website: "https://www.ashfordsales.ca/",
-    location: "Mount Pearl, NL",
-  },
-  "Honda One": {
-    name: "Honda One",
-    logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://hondaone.net/en/",
-    location: "Mount Pearl, NL",
-  },
-  "Pardys Recreation": {
-    name: "Pardys Recreation",
-    logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://www.pardysrecreationandmarine.com/",
-    location: "Marystown, NL",
-  },
-  "Mobile Trailer Repair": {
-    name: "Mobile Trailer Repair",
-    logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://mobiletrailer.ca/",
-    location: "Mount Pearl, NL",
-  },
-  "The Hot Tub & Sauna Depot": {
-    name: "The Hot Tub & Sauna Depot",
-    logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://hottubandsaunadepot.com/",
     location: "Mount Pearl, NL",
   },
   "Aurora Holdings": {
@@ -61,11 +11,53 @@ const dealers = {
     website: "https://auroraequipmentsales.ca/",
     location: "Stanhope, NL",
   },
-  "Wisemans Sales & Service": {
-    name: "Wisemans Sales & Service",
+  "Blue Water Marine": {
+    name: "Blue Water Marine",
+    logo: "/images/dealers/blue-water-marine.avif",
+    website: "https://bluewatermarine.ca/",
+    location: "Mount Pearl, NL",
+  },
+  "Honda One": {
+    name: "Honda One",
     logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://wisemansales.com/",
-    location: "Clarenville, NL",
+    website: "https://hondaone.net/en/",
+    location: "Mount Pearl, NL",
+  },
+  "Marble RV": {
+    name: "Marble RV",
+    logo: "/images/dealers/nlp-finance-marketplace.png",
+    website: "https://marblerv.ca/",
+    location: "Corner Brook, NL",
+  },
+  "MF Motorsports": {
+    name: "MF Motorsports",
+    logo: "/images/dealers/mf-motors.png",
+    website: "https://www.mfmotorsltd.com/",
+    location: "Stephenville, NL",
+  },
+  "Mid Island Motorsports": {
+    name: "Mid Island Motorsports",
+    logo: "/images/dealers/mid-island-motorsports.png",
+    website: "https://www.midislandmotorsports.com/",
+    location: "Springdale, NL",
+  },
+  "Mobile Trailer Repair": {
+    name: "Mobile Trailer Repair",
+    logo: "/images/dealers/nlp-finance-marketplace.png",
+    website: "https://mobiletrailer.ca/",
+    location: "Mount Pearl, NL",
+  },
+  "NLP Finance Marketplace": {
+    name: "NLP Finance Marketplace",
+    logo: "/images/dealers/nlp-finance-marketplace.png",
+    website: "https://nlpfinance.ca/",
+    location: "Grand Falls-Windsor, NL",
+  },
+  "Pardys Recreation": {
+    name: "Pardys Recreation",
+    logo: "/images/dealers/nlp-finance-marketplace.png",
+    website: "https://www.pardysrecreationandmarine.com/",
+    location: "Marystown, NL",
   },
   "Route 460": {
     name: "Route 460",
@@ -73,11 +65,17 @@ const dealers = {
     website: "https://www.route460.com/",
     location: "Stephenville, NL",
   },
-  "Marble RV": {
-    name: "Marble RV",
+  "The Hot Tub & Sauna Depot": {
+    name: "The Hot Tub & Sauna Depot",
     logo: "/images/dealers/nlp-finance-marketplace.png",
-    website: "https://marblerv.ca/",
-    location: "Corner Brook, NL",
+    website: "https://hottubandsaunadepot.com/",
+    location: "Mount Pearl, NL",
+  },
+  "Wisemans Sales & Service": {
+    name: "Wisemans Sales & Service",
+    logo: "/images/dealers/nlp-finance-marketplace.png",
+    website: "https://wisemansales.com/",
+    location: "Clarenville, NL",
   },
 };
 
