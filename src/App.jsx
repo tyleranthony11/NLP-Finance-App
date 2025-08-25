8ccc534a758b92feea5fc54169973e0887b3c08f
tyleranthony11 <tmanthony@mun.ca>
2025-08-25 11:54:55 -0230

    tmep login

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ function App() {
             }
           />
 
-          <Route path="/dashboard/*" element={<Dashboard />} />
+          <Route path="/dashboard/*" element={<Dashboard user={user} />} />
         </Routes>
       </Router>
     </div>
